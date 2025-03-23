bfe5fe2d4d8d48b30d580f9e9710105681b0e398
User <125206457+ghostuser-bug@users.noreply.github.com>
2025-03-23 16:47:21 +0800

    Delete Gadget_Store directory

--- Gadget_Store/admin_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove all session variables
-session_destroy(); // Destroy the session
-header('Location: admin_login.php');
-exit;
-
--- Gadget_Store/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.btn-success').addEventListener('click', () => {
-    alert('Search feature is coming soon!');
-  });
-  var myModal = new bootstrap.Modal(document.getElementById('paymentModal'));
-myModal.hide();
